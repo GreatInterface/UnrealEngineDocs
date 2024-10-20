24b284d20de40bf0dd832a6b1fbbcb63ed8b9ca5
GreatInterface <C2931597944@outlook.com>
2024-10-20 14:56:26 +0800

    first commit

---
+++ .vitepress/config.mjs
@@ -0,0 +1,62 @@
+import { defineConfig } from 'vitepress'
+//import { SetSidebar } from '/utils/auto_sidebar.mjs'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Welcome to Kyro Unreal Engine Blog",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+
+    //支持md格式2到6级标题
+    outline: {
+      label: "目录",
+      level: [2,6]
+    },
+
+    nav: [
+      { text: '杂项', link: '/' },
+
+      {text: 'C++', items: [
+          { text:'Template', link: '/markdown-examples'}
+        ] },
+
+      { text: 'Unreal Engine',
+        items: [
+          { text: 'Lyra', items:[
+              {text: 'Interaction', link: '/markdown-examples'}
+            ]}
+        ] }
+    ],
+
+    //为了支持两边栏
+    sidebar: false, //关闭三边栏中的侧边栏
+    aside: "left",  //设置右边侧边栏在左侧显示
+
+    // sidebar: [
+    //   {
+    //     text: 'Examples',
+    //     items: [
+    //       { text: 'Markdown Examples', link: '/markdown-examples' },
+    //       { text: 'Runtime API Examples', link: '/api-examples' }
+    //     ]
+    //   }
+    // ],
+
+    // 支持自动化sidebar
+    // sidebar:{
+    //   "/Context/Unreal Engine" : SetSidebar("/Context/Unreal Engine"),
+    //
+    // },
+
+    //socialLinks: [
+    //  { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+   // ],
+
+    //底部配置
+    footer: {
+      copyright:"Kyro @ 2024-Present"
+    }
+  }
+
+})
---
+++ index.md
@@ -0,0 +1,23 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+  name: "Unreal Engine"
+  text: "Blogs and Tips"
+  tagline: Explore, Share, and Innovate
+  actions:
+    - theme: brand
+      text: Begin
+      link: /markdown-examples
+    - theme: alt
+      text: API Examples
+      link: /api-examples
+
+features:
+  - title: 吾日三省吾身
+    details: 今天有学习了吗？今天有思考吗？今天有实践吗？
+  - title: Lyra
+    details: 分析与总结
+---
+
