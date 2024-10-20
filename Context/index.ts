8f3a545cb7cf91b4cf045b8e2261faff797ca822
GreatInterface <C2931597944@outlook.com>
2024-10-20 22:24:03 +0800

    .

---
+++ Context/index.ts
@@ -0,0 +1,36 @@
+﻿interface navItem{
+    id: string | number;
+    text: string;
+    desc?: string;
+    link: string;
+    icon?: string;
+}
+
+const CPP: navItem[] = [
+    {
+        id: 1,
+        text: "",
+        desc: "",
+        link: "",
+        icon: "",
+    },
+];
+
+const UnrealEngine: navItem[] = [
+    {
+        id: 1,
+        text: "Lyra",
+        desc: "官方演示项目",
+        link: "./Unreal Engine/Lyra/",
+        icon: "",
+    },
+    {
+        id: 2,
+        text: "Core",
+        desc: "Runtime/Core",
+        link: "./Unreal Engine/Core/",
+        icon: "",
+    },
+];
+
+export { CPP, UnrealEngine };
