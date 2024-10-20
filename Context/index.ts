ed8ed6c283602d9fd858150b2bd426d4e437c087
GreatInterface <C2931597944@outlook.com>
2024-10-20 22:46:36 +0800

    .

--- Context/index.ts
+++ Context/index.ts
@@ -9,28 +9,38 @@
 const CPP: navItem[] = [
     {
         id: 1,
-        text: "",
-        desc: "",
-        link: "",
-        icon: "",
+        text: "css奇思妙想",
+        link: "http://chokcoco.github.io/magicCss/html/index.html",
+        icon: "https://www.91sotu.com/logo.png",
+        desc: "CSS3 奇思妙想，使用 CSS3 在单个标签内实现各类图形。",
     },
 ];
 
 const UnrealEngine: navItem[] = [
     {
         id: 1,
-        text: "Lyra",
-        desc: "官方演示项目",
-        link: "./Unreal Engine/Lyra/",
-        icon: "",
+        text: "css奇思妙想",
+        link: "http://chokcoco.github.io/magicCss/html/index.html",
+        icon: "https://www.91sotu.com/logo.png",
+        desc: "CSS3 奇思妙想，使用 CSS3 在单个标签内实现各类图形。",
     },
+
     {
         id: 2,
         text: "Core",
         desc: "Runtime/Core",
-        link: "./Unreal Engine/Core/",
+        link: "/UnrealEngine/Core/",
         icon: "",
     },
+
+    {
+        id: 3,
+        text: "Lyra",
+        desc: "官方演示项目",
+        link: "/UnrealEngine/Lyra/",
+        icon: "",
+    },
+
 ];
 
 export { CPP, UnrealEngine };
